dea9de057aa3fca8fdc7a3e2f84f0fb4f09bcc32
boomzero <thomas_rainbowfish@icloud.com>
2023-09-29 09:06:50 +0800

    fix

--- ojsys/views.py
+++ ojsys/views.py
@@ -44,9 +44,7 @@ def submit(request, question_id):
             },
         )
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("ojsys:results", args=(question.id,)))
